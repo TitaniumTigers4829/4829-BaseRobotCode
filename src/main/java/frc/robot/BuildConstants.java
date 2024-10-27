77f11653ff4f9025e250a23c7b369ca3c68e7605
Ishan1522 <ishanacharyagangopadhyay@gmail.com>
2024-10-27 13:39:33 -0400

    comment out broken build thingy

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "4829-BaseRobotCode-2025-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "f8e3184c6cd92ce61523fd880dee86fd1c9748a3";
-  public static final String GIT_DATE = "2024-10-27 12:26:14 EDT";
+  public static final int GIT_REVISION = 19;
+  public static final String GIT_SHA = "184342ec3f171f4c5ecbeba163bafd4535ed39be";
+  public static final String GIT_DATE = "2024-10-27 13:35:52 EDT";
   public static final String GIT_BRANCH = "k";
-  public static final String BUILD_DATE = "2024-10-27 12:33:58 EDT";
-  public static final long BUILD_UNIX_TIME = 1730046838310L;
+  public static final String BUILD_DATE = "2024-10-27 13:38:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1730050714303L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
