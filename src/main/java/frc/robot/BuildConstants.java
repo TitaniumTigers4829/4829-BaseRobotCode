0d45bba6ffc24373aef17c97030333ba903faf7f
Ishan <98932677+Ishan1522@users.noreply.github.com>
2024-11-05 08:00:30 -0500

    heck me

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -1,17 +1,19 @@
 package frc.robot;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "4829-BaseRobotCode-1";
+  public static final String MAVEN_NAME = "4829-BaseRobotCode";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 32;
-  public static final String GIT_SHA = "71a47d311fd066ba3b76be8d09eff3e03b893cd8";
-  public static final String GIT_DATE = "2024-10-28 22:21:12 EDT";
-  public static final String GIT_BRANCH = "docs";
-  public static final String BUILD_DATE = "2024-10-29 16:56:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1730235405242L;
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "52fbf88798bd786d25dffff6b4769182ad9ea51c";
+  public static final String GIT_DATE = "2024-10-29 17:58:37 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-11-05 07:52:13 EST";
+  public static final long BUILD_UNIX_TIME = 1730811133569L;
   public static final int DIRTY = 0;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
