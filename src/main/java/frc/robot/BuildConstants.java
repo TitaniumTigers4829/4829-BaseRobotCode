27c9f2c282ae83400017790b6625ce5eda704363
Ishan <98932677+Ishan1522@users.noreply.github.com>
2025-01-04 03:24:36 -0500

    merge example fr this time (#23) & attempt co-authoring
    * delete unused stuff
    
    * Refactor SwerveDrive to use DriveConstants for kinematics and remove unused test files
    
    * import stuff
    
    Co-authored-by: Jack <JacksonElia@users.noreply.github.com>
    
    * simulation stuff
    
    * format
    
    * noice
    
    * format
    
    * added some of the more important docstrings.. the rest will be added later
    
    * Examples yay (#21)
    
    * maybe it will work
    
    * Added more intake stuff and made it finally build!
    
    * intake commands
    
    * Cool elevator stuff
    
    * more elevator stuff
    
    * Hopefully i finish this soon
    
    * idk how that got in there
    
    * Fun arm things
    
    * format
    
    * Remove unnecessary blank line in MathUtil.java
    
    * nerf intake to not use position for otb intake. delete arm example bc it is redundant with elevator existing
    
    * format i think?
    
    * update beta version
    
    ---------
    
    Co-authored-by: Jack <JacksonElia@users.noreply.github.com>
    Co-authored-by: Max Luna <Urbanmass@users.noreply.github.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,11 +6,11 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "4829-BaseRobotCode";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "f6cf5383d95d1116029cbb69e2db020295f5c2bf";
-  public static final String GIT_DATE = "2024-11-09 19:29:39 EST";
-  public static final String GIT_BRANCH = "actual-unit-tests";
-  public static final String BUILD_DATE = "2024-11-09 19:50:39 EST";
-  public static final long BUILD_UNIX_TIME = 1731199839115L;
+  public static final String GIT_SHA = "9b2d52efc94f51550c86781dc4d51112afc44b13";
+  public static final String GIT_DATE = "2024-12-16 17:53:23 EST";
+  public static final String GIT_BRANCH = "ExamplesYay";
+  public static final String BUILD_DATE = "2024-12-18 17:41:48 EST";
+  public static final long BUILD_UNIX_TIME = 1734561708416L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
